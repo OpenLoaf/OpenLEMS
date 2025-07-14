3b7ca2901ee8c3198f69d6dcf3d3feb9cf5029ae
孔博强 <m17717692780@163.com>
2025-07-14 18:14:10 +0800

    更新SDriver结构，调整DriverStatus字段的描述为“运行状态”，提升代码可读性。同时注释掉SDriverDetail结构，准备后续开发。

--- application/internal/model/entity/driver.go
+++ application/internal/model/entity/driver.go
@@ -5,7 +5,18 @@ type SDriver struct {
 	DriverVersion     string `json:"driverVersion" dc:"版本号"`
 	DriverType        string `json:"driverType" dc:"设备类型"`
 	DriverDescription string `json:"driverDescription" dc:"驱动描述"`
-	DriverStatus      bool   `json:"driverStatus" dc:"状态"`
+	DriverStatus      bool   `json:"driverStatus" dc:"运行状态"`
 	DriverLastUpdate  string `json:"driverLastUpdate" dc:"更新时间	"`
 	ProtocolType      string `json:"protocolType" dc:"协议类型"`
 }
+
+// type SDriverDetail struct {
+// 	DriverName        string `json:"driverName" dc:"驱动名称"`
+// 	DriverVersion     string `json:"driverVersion" dc:"版本号"`
+// 	DriverType        string `json:"driverType" dc:"设备类型"`
+// 	DriverDescription string `json:"driverDescription" dc:"驱动描述"`
+// 	DriverStatus      bool   `json:"driverStatus" dc:"运行状态"`
+// 	DriverLastUpdate  string `json:"driverLastUpdate" dc:"更新时间	"`
+// 	ProtocolType      string `json:"protocolType" dc:"协议类型"`
+
+// }
