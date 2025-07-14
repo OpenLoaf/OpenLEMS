fc0a939e9f0d3232f8ecb1f37779682ebd5bf435
孔博强 <m17717692780@163.com>
2025-07-14 10:45:15 +0800

    修改驱动状态字段类型为布尔值，以更准确地表示驱动状态，提升数据结构的清晰度与一致性。

--- application/internal/model/entity/driver.go
+++ application/internal/model/entity/driver.go
@@ -5,6 +5,6 @@ type SDriver struct {
 	DriverVersion      string `json:"driverVersion" dc:"版本号"`
 	DriverProtocolType string `json:"driverProtocolType" dc:"协议类型"`
 	DriverDescription  string `json:"driverDescription" dc:"驱动描述"`
-	DriverStatus       string `json:"driverStatus" dc:"状态"`
+	DriverStatus       bool   `json:"driverStatus" dc:"状态"`
 	DriverLastUpdate   string `json:"driverLastUpdate" dc:"更新时间	"`
 }
