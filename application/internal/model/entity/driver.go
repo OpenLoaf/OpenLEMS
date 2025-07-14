19e61a64e79106f936c589b5638759109ea13ebc
孔博强 <m17717692780@163.com>
2025-07-14 14:02:46 +0800

    更新SDriver结构，新增DriverType字段以支持设备类型，调整ProtocolType字段位置，提升代码可读性与扩展性。

--- application/internal/model/entity/driver.go
+++ application/internal/model/entity/driver.go
@@ -1,10 +1,11 @@
 package entity
 
 type SDriver struct {
-	DriverName         string `json:"driverName" dc:"驱动名称"`
-	DriverVersion      string `json:"driverVersion" dc:"版本号"`
-	DriverProtocolType string `json:"driverProtocolType" dc:"协议类型"`
-	DriverDescription  string `json:"driverDescription" dc:"驱动描述"`
-	DriverStatus       bool   `json:"driverStatus" dc:"状态"`
-	DriverLastUpdate   string `json:"driverLastUpdate" dc:"更新时间	"`
+	DriverName        string `json:"driverName" dc:"驱动名称"`
+	DriverVersion     string `json:"driverVersion" dc:"版本号"`
+	DriverType        string `json:"driverType" dc:"设备类型"`
+	DriverDescription string `json:"driverDescription" dc:"驱动描述"`
+	DriverStatus      bool   `json:"driverStatus" dc:"状态"`
+	DriverLastUpdate  string `json:"driverLastUpdate" dc:"更新时间	"`
+	ProtocolType      string `json:"protocolType" dc:"协议类型"`
 }
