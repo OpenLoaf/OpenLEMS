86770053225845be34d68f22488b0f94fe7be838
孔博强 <m17717692780@163.com>
2025-07-14 18:13:57 +0800

    新增GetDriver方法，支持根据请求获取驱动信息，提升驱动管理功能的灵活性与可扩展性。

---
+++ application/internal/controller/driver/driver_v1_get_driver.go
@@ -0,0 +1,12 @@
+package driver
+
+import (
+	"context"
+
+	v1 "application/api/driver/v1"
+)
+
+func (c *ControllerV1) GetDriver(ctx context.Context, req *v1.GetDriverReq) (res *v1.GetDriverRes, err error) {
+
+	return nil, nil
+}
