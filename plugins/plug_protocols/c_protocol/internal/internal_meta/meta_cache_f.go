cb39965201f0ddc21face7fa043a2118d7c49554
zz@hexems.com <zhaozhen@hexems.com>
2025-08-21 16:27:22 +0800

    调整点位顺序

--- plugins/plug_protocols/c_protocol/internal/internal_meta/meta_cache_f.go
+++ plugins/plug_protocols/c_protocol/internal/internal_meta/meta_cache_f.go
@@ -53,7 +53,7 @@ func CacheValue(ctx context.Context, deviceId string, deviceType c_base.EDeviceT
 	//g.Log().Debugf(ctx, "[%s-%s] 值: %v cache is null:%v", deviceId, meta.Cn, value, cache == nil)
 
 	if meta.Debug {
-		g.Log().Debugf(ctx, "[%s-%s] 值: %v", deviceId, meta.Cn, value)
+		g.Log().Infof(ctx, "[deviceId:%s-%s] 值: %v", deviceId, meta.Cn, value)
 	}
 
 	return value, nil
