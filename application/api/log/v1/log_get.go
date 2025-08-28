1cf949130685b1372e24e86eb112a03bd468fb4d
zz@hexems.com <zhaozhen@hexems.com>
2025-08-28 15:03:54 +0800

    更新日志接口日期格式为 `2006-01-02`，统一格式定义以提升一致性。

--- application/api/log/v1/log_get.go
+++ application/api/log/v1/log_get.go
@@ -9,7 +9,7 @@ type GetBizLogReq struct {
 	g.Meta   `path:"/log/biz" method:"get" tags:"日志" summary:"读取业务日志"`
 	Type     string `json:"type"   v:"in:ems,device,protocol,policy,all" dc:"业务类型(可空/为all返回全部)"`
 	Id       string `json:"id"     dc:"相关ID(ems可空)"`
-	Date     string `json:"date"   dc:"日期，格式：20060102，默认为今天"`
+	Date     string `json:"date"   dc:"日期，格式：2006-01-02，默认为今天"`
 	Page     int    `json:"page"   d:"1" dc:"页码，从1开始"`
 	PageSize int    `json:"pageSize" d:"100" dc:"每页条数(最大1000)"`
 	Level    string `json:"level"  v:"in:DEBUG,INFO,WARN,ERROR,ALL" dc:"日志等级(可空/为ALL返回全部；仅支持: DEBUG/INFO/WARN/ERROR)"`
