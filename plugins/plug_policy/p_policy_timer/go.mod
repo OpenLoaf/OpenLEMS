1b9558b0cdd11465daceb5a00e6ced0da54bc28b
zz@hexems.com <zhaozhen@hexems.com>
2025-09-08 23:29:33 +0800

    删除无用的微网预测器和定时器策略相关代码，清理项目结构。

--- plugins/plug_policy/p_policy_timer/go.mod
+++
@@ -1,3 +0,0 @@
-module p_timer_policy
-
-go 1.25
--- plugins/plug_policy/p_policy_timer/internal/p_test_i.go
+++
@@ -1,8 +0,0 @@
-package internal
-
-type ITest interface {
-	GetProviderPower() (float64, error)          // 获取发电功率
-	GetConsumerPower() (float64, error)          // 获取用电功率
-	GetFixedSafeCapacityPower() (float64, error) // 获取固定的安全功率(容量功率）
-
-}
