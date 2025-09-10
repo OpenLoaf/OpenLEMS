d4a7d94ade264397bb79181178ec3c1d1b0c2e50
zz@hexems.com <zhaozhen@hexems.com>
2025-09-10 23:39:49 +0800

    重构SPointGroup结构体定义，新增GroupKey字段并标记为必填，优化GroupName字段验证规则，明确字段可重复性说明，提升点位组配置的灵活性和规范性。

--- common/c_base/c_base_point_group_s.go
+++ common/c_base/c_base_point_group_s.go
@@ -1,7 +1,8 @@
 package c_base
 
 type SPointGroup struct {
-	GroupName string `json:"groupName" dc:"组名称"`
+	GroupKey  string `json:"groupKey" v:"required" dc:"组key"` // key是可以重复
+	GroupName string `json:"groupName" v:"required" dc:"组名称"` // 名字可以重复
 	GroupSort int    `json:"groupSort" dc:"组排序"`
 	Display   bool   `json:"display" dc:"是否显示"` // todo  这个字段暂时没用，先留着
 }
