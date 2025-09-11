5fa2c9a4146fbed244c64bc8eee3b0a66ab6d34d
孔博强 <your.email@example.com>
2025-09-11 14:31:23 +0800

    新增日志查询功能，支持获取业务日志信息和统计数据，优化日志管理接口，提升代码的灵活性和可维护性。

--- application/api/log/log.go
+++ application/api/log/log.go
@@ -13,4 +13,6 @@ import (
 type ILogV1 interface {
 	GetBizLog(ctx context.Context, req *v1.GetBizLogReq) (res *v1.GetBizLogRes, err error)
 	DeleteBizLog(ctx context.Context, req *v1.DeleteBizLogReq) (res *v1.DeleteBizLogRes, err error)
+	QueryBizLogInfo(ctx context.Context, req *v1.QueryBizLogInfoReq) (res *v1.QueryBizLogInfoRes, err error)
+	QueryBizLogStatistics(ctx context.Context, req *v1.QueryBizLogStatisticsReq) (res *v1.QueryBizLogStatisticsRes, err error)
 }
---
+++ application/api/log/v1/log_query_info.go
@@ -0,0 +1,15 @@
+package v1
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+)
+
+type QueryBizLogInfoReq struct {
+	g.Meta `path:"/log/biz/info" method:"get" tags:"日志" summary:"查询日志信息"`
+}
+
+type QueryBizLogInfoRes struct {
+	Total         int    `json:"total" dc:"日志总数"`
+	FirstLogTime  string `json:"firstLogTime" dc:"第一条日志时间"`
+	LatestLogTime string `json:"latestLogTime" dc:"最新日志时间"`
+}
