fecb3ce8cc11d8bfd61d8e1d322f1687a2c981ce
zz@hexems.com <zhaozhen@hexems.com>
2025-09-17 17:47:09 +0800

    调整设备控制参数类型为切片形式：
    1. 将 `ControlDeviceReq` 中 `Parameters` 类型从 `map[string]interface{}` 调整为 `[]string`；
    2. 优化参数定义以增强数据一致性与解析灵活性；

--- application/api/control/v1/control_device.go
+++ application/api/control/v1/control_device.go
@@ -8,9 +8,9 @@ import (
 
 type ControlDeviceReq struct {
 	g.Meta      `path:"/control/device" method:"post" tags:"设备控制" summary:"控制设备"`
-	DeviceId    string                 `json:"deviceId" v:"required" dc:"设备ID"`
-	CommandName string                 `json:"commandName" v:"required" dc:"指令名称"`
-	Parameters  map[string]interface{} `json:"parameters" dc:"参数值"`
+	DeviceId    string   `json:"deviceId" v:"required" dc:"设备ID"`
+	CommandName string   `json:"commandName" v:"required" dc:"指令名称"`
+	Parameters  []string `json:"parameters" dc:"参数值"`
 }
 
 type ControlDeviceRes struct {
