a435d16133ff40c30b1a05800b46a4c23a2d9273
zz@hexems.com <zhaozhen@hexems.com>
2025-09-18 17:41:08 +0800

    Removed unused `icon` fields from telemetry and streamlined `build.yaml` and service structures across multiple plugins. Updated documentation to reflect these changes.

--- common/c_base/c_base_driver_service_s.go
+++ common/c_base/c_base_driver_service_s.go
@@ -2,8 +2,16 @@ package c_base
 
 import "common/c_enum"
 
+type SDriverService struct {
+	Icon        string `yaml:"icon,omitempty" json:"icon" dc:"图标"`
+	Key         string `json:"key" yaml:"key" dc:"执行的方法名"`
+	Name        string `json:"name" yaml:"name"  dc:"服务名称，支持i18n"`
+	Description string `json:"description" yaml:"description" dc:"备注"`
+	//Service     func(any) error `json:"-" dc:"执行方法"`
+	Params []*SConfigStructFields `json:"params" yaml:"params" dc:"参数定义"`
+}
+
 type STelemetry struct {
-	Icon         string               `yaml:"icon,omitempty" json:"icon" dc:"图标"`
 	Key          string               `json:"key,omitempty" yaml:"key"`                   // 遥测名称
 	Name         string               `json:"name" yaml:"name"`                           // 遥测名称的国际化覆盖
 	Precise      uint8                `json:"precise" yaml:"precise"`                     // 浮点数精度，默认0
@@ -39,12 +47,4 @@ func (s *STelemetry) ToPoint(valueType c_enum.EValueType, params map[string]any)
 		ValueType:    valueType,
 		ValueExplain: valueExplain,
 	}
-}
-
-type SDriverService struct {
-	Key         string `json:"key" yaml:"key" dc:"执行的方法名"`
-	Name        string `json:"name" yaml:"name"  dc:"服务名称，支持i18n"`
-	Description string `json:"description" yaml:"description" dc:"备注"`
-	//Service     func(any) error `json:"-" dc:"执行方法"`
-	Params []*SConfigStructFields `json:"params" yaml:"params" dc:"参数定义"`
 }
