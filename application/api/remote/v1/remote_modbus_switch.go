c63c4acdc5a8e983048db082a47355e6e8c11d93
zz@hexems.com <zhaozhen@hexems.com>
2025-10-13 10:42:44 +0800

    Add MqttSwitch and ModbusSwitch methods to IRemoteV1 interface
    - Introduced `MqttSwitch` method to facilitate MQTT service switching functionality.
    - Added `ModbusSwitch` method to enable Modbus service switching capabilities.
    - These additions enhance the API's ability to manage service states effectively, improving overall system control.

---
+++ application/api/remote/v1/remote_modbus_switch.go
@@ -0,0 +1,13 @@
+package v1
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+)
+
+type ModbusSwitchReq struct {
+	g.Meta  `path:"/remote/modbus/enabled/{enabled}" method:"put" tags:"远程管理" summary:"Modbus服务开关"`
+	Enabled bool `json:"enabled" dc:"是否启用Modbus服务" v:"required#启用状态不能为空"`
+}
+
+type ModbusSwitchRes struct {
+}
---
+++ application/api/remote/v1/remote_mqtt_switch.go
@@ -0,0 +1,13 @@
+package v1
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+)
+
+type MqttSwitchReq struct {
+	g.Meta  `path:"/remote/mqtt/enabled/{enabled}" method:"put" tags:"远程管理" summary:"MQTT服务开关"`
+	Enabled bool `json:"enabled" dc:"是否启用MQTT服务" v:"required#启用状态不能为空"`
+}
+
+type MqttSwitchRes struct {
+}
