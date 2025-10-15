bc5d774ff901a4218b60927c59a148712326ea44
zz@hexems.com <zhaozhen@hexems.com>
2025-10-15 18:38:18 +0800

    优化 ClearAlarmReq 结构
    - 移除 `ClearAlarmReq` 中对告警级别参数的必填校验规则，简化参数验证逻辑。

--- application/api/alarm/v1/alarm_clear.go
+++ application/api/alarm/v1/alarm_clear.go
@@ -8,7 +8,7 @@ import (
 
 type ClearAlarmReq struct {
 	g.Meta   `path:"/alarm/clear" method:"post" tags:"告警" summary:"清除告警"`
-	Level    string `json:"level" v:"required|in:None,Warn,Alert,Error" dc:"告警类型，必填"`
+	Level    string `json:"level" v:"in:None,Warn,Alert,Error" dc:"告警类型，必填"`
 	DeviceId string `json:"deviceId" dc:"设备ID，可选；为空则清除所有设备的指定类型告警"`
 }
 
