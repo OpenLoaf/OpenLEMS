330c80a3fb1bc2a58b165a1fbb5f33d072c22a10
zz@hexems.com <zhaozhen@hexems.com>
2025-10-22 00:08:38 +0800

    update

---
+++ application/manifest/web/assets/driver-C7j54_TS.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-I6bU3Cg0.js";const i=()=>r("/driver/list"),s=t=>r("/driver/get",{driverName:t});export{s as a,i as g};
---
+++ application/manifest/web/assets/setting-DTq1DYwV.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-I6bU3Cg0.js";const s=t=>e("/setting/detail",t),a=t=>e("/setting/update",t);export{s as g,a as u};
