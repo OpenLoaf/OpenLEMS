2db71c07bb14b24d2eefe7ccf424570a867a05cc
zz@hexems.com <zhaozhen@hexems.com>
2025-10-22 14:25:14 +0800

    增强协议删除和更新操作的日志记录功能，添加设备管理器重启逻辑以应用更改

--- application/internal/controller/protocol/protocol_v1_delete_protocol.go
+++ application/internal/controller/protocol/protocol_v1_delete_protocol.go
@@ -1,21 +1,40 @@
 package protocol
 
 import (
+	"common"
+	"common/c_log"
 	"context"
-	"github.com/gogf/gf/v2/errors/gerror"
 	"s_db"
 
+	"github.com/gogf/gf/v2/errors/gerror"
+
 	v1 "application/api/protocol/v1"
+
 	"github.com/gogf/gf/v2/errors/gcode"
 )
 
 func (c *ControllerV1) DeleteProtocol(ctx context.Context, req *v1.DeleteProtocolReq) (res *v1.DeleteProtocolRes, err error) {
+	// 记录业务操作开始
+	c_log.BizInfo(ctx, "协议删除操作开始", "protocolId", req.ProtocolId)
 
 	// 删除协议
 	err = s_db.GetProtocolService().DeleteProtocol(ctx, req.ProtocolId)
 	if err != nil {
+		c_log.BizError(ctx, "协议删除失败", "protocolId", req.ProtocolId, "error", err)
 		return nil, gerror.WrapCode(gcode.CodeInternalError, err, "删除协议失败")
 	}
+
+	// 记录协议删除需要重启设备服务
+	c_log.BizInfo(ctx, "协议删除需要重启设备服务", "protocolId", req.ProtocolId)
+
+	// 重启设备管理器以应用协议删除更改
+	common.GetDeviceManager().Shutdown()
+	common.GetDeviceManager().Start()
+
+	c_log.BizInfo(ctx, "设备服务重启完成", "protocolId", req.ProtocolId)
+
+	// 记录业务操作成功
+	c_log.BizInfo(ctx, "协议删除操作成功", "protocolId", req.ProtocolId)
 
 	res = &v1.DeleteProtocolRes{
 		ProtocolId: req.ProtocolId,
--- application/internal/controller/protocol/protocol_v1_update_protocol.go
+++ application/internal/controller/protocol/protocol_v1_update_protocol.go
@@ -1,6 +1,8 @@
 package protocol
 
 import (
+	"common"
+	"common/c_log"
 	"context"
 	"s_db"
 
@@ -8,6 +10,9 @@ import (
 )
 
 func (c *ControllerV1) UpdateProtocol(ctx context.Context, req *v1.UpdateProtocolReq) (res *v1.UpdateProtocolRes, err error) {
+	// 记录业务操作开始
+	c_log.BizInfo(ctx, "协议更新操作开始", "protocolId", req.ProtocolId)
+
 	// 将请求参数构造为 map
 	updateData := map[string]interface{}{
 		"protocolName":     req.ProtocolName,
@@ -21,8 +26,21 @@ func (c *ControllerV1) UpdateProtocol(ctx context.Context, req *v1.UpdateProtoco
 
 	err = s_db.GetProtocolService().UpdateProtocol(ctx, req.ProtocolId, updateData)
 	if err != nil {
+		c_log.BizError(ctx, "协议更新失败", "protocolId", req.ProtocolId, "error", err)
 		return nil, err
 	}
+
+	// 记录协议配置变更需要重启设备服务
+	c_log.BizInfo(ctx, "协议配置变更需要重启设备服务", "protocolId", req.ProtocolId)
+
+	// 重启设备管理器以应用协议配置更改
+	common.GetDeviceManager().Shutdown()
+	common.GetDeviceManager().Start()
+
+	c_log.BizInfo(ctx, "设备服务重启完成", "protocolId", req.ProtocolId)
+
+	// 记录业务操作成功
+	c_log.BizInfo(ctx, "协议更新操作成功", "protocolId", req.ProtocolId)
 
 	res = &v1.UpdateProtocolRes{
 		ProtocolId: req.ProtocolId,
